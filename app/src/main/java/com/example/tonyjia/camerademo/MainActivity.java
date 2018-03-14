50a3a69c1062b9b0ee811c4bdfa6f573ec8b3ca2
jiazhiqiang <jiaziqiang@tzpt.com>
2018-03-14 19:25:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tonyjia/camerademo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tonyjia.camerademo;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_IMMERSIVE |
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        }
+        setContentView(R.layout.activity_main);
+    }
+}
